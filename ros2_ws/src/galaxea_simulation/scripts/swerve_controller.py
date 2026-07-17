69a5bd8a869ba0d9c63cdb0a003a9da55115acef
WongKahLok <wong87321@gmail.com>
2026-07-17 21:20:37 +0800

    Revert "fix: robot moved in a slanted line even though commanded forward"
    This reverts commit 7c836f474b02cf3ff1f457fb86ce4592cf4b9a9b.

--- ros2_ws/src/galaxea_simulation/scripts/swerve_controller.py
+++ ros2_ws/src/galaxea_simulation/scripts/swerve_controller.py
@@ -111,12 +111,7 @@ class SwerveController(Node):
             speed = math.hypot(mx, my)
 
             if speed < self.deadband:
-                # Recenter to straight-ahead while stopped instead of freezing at
-                # whatever angle the module last had. Otherwise a module left
-                # parked off-axis after a turn/strafe has to servo that whole
-                # distance back once the next drive command arrives, and the
-                # robot visibly swerves during that catch-up window.
-                angle = 0.0
+                angle = self.current_steer[i]
                 wheel_vel = 0.0
             else:
                 angle = math.atan2(my, mx)
